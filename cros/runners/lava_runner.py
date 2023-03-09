66bfd2954c97a0055a35a6c09b7b53379671ffdb
Tzung-Bi Shih <tzungbi@kernel.org>
2023-03-09 16:30:50 +0800

    lava_runner: use TextTestRunner
    To simplify, eliminate LavaTestRunner but use TextTestRunner directly.
    Also change the constructor of LavaTestResult per [1].
    
    As a side-effect result, it starts to show the following summary:
    
    > Ran 18 tests in 0.010s
    >
    > FAILED (failures=1, errors=3, skipped=10)
    
    [1]: https://docs.python.org/3/library/unittest.html#unittest.TextTestRunner._makeResult
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>

--- cros/runners/lava_runner.py
+++ cros/runners/lava_runner.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import sys
 import unittest
 
 from cros.tests.cros_ec_accel import *
@@ -14,53 +13,38 @@ from cros.tests.cros_ec_extcon import *
 
 
 class LavaTestResult(unittest.TestResult):
-    def __init__(self, runner, verbosity=0):
-        super().__init__()
-        self.runner = runner
+    def __init__(self, stream, descriptions, verbosity):
+        super().__init__(stream, descriptions, verbosity)
+        self.stream = stream
 
     def addSuccess(self, test):
         super().addSuccess(test)
         testcase = test.id().rsplit(".")[-1]
-        self.runner.writeUpdate(
+        self.stream.write(
             f"<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={testcase} RESULT=pass>\n")
 
     def addError(self, test, err):
         super().addError(test, err)
         testcase = test.id().rsplit(".")[-1]
-        self.runner.writeUpdate(
+        self.stream.write(
             f"<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={testcase} RESULT=unknown>\n")
 
     def addFailure(self, test, err):
         super().addFailure(test, err)
         testcase = test.id().rsplit(".")[-1]
-        self.runner.writeUpdate(
+        self.stream.write(
             f"<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={testcase} RESULT=fail>\n")
 
     def addSkip(self, test, reason):
         super().addSkip(test, reason)
         testcase = test.id().rsplit(".")[-1]
-        self.runner.writeUpdate(
+        self.stream.write(
             f"<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={testcase} RESULT=skip>\n")
-
-
-class LavaTestRunner:
-    def __init__(self, stream=sys.stderr, verbosity=0):
-        self.stream = stream
-        self.verbosity = verbosity
-
-    def writeUpdate(self, message):
-        self.stream.write(message)
-
-    def run(self, test):
-        result = LavaTestResult(self, self.verbosity)
-        test(result)
-        result.testsRun
-        return result
 
 
 if __name__ == "__main__":
     unittest.main(
-        testRunner=LavaTestRunner(),
+        testRunner=unittest.TextTestRunner(resultclass=LavaTestResult),
         # these make sure that some options that are not applicable
         # remain hidden from the help menu.
         failfast=False,
