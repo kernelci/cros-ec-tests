2dc3bfa22cea00dd23555aa900bad658f639f1eb
Tzung-Bi Shih <tzungbi@kernel.org>
2023-03-14 15:12:32 +0800

    lava_runner: remove --verbosity
    This partially reverts 8810aca3ccc7 ("Add a verbosity flag to add more
    error context in outputs").
    
    As unittest framework already provided ways to specify verbosity, remove
    --verbosity flag.
    
    Also, remove the code prints errors and fails as TextTestResult already
    did similar things as we are moving forward to use it.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>

--- cros/runners/lava_runner.py
+++ cros/runners/lava_runner.py
@@ -3,7 +3,6 @@
 
 import sys
 import unittest
-import traceback
 
 from cros.tests.cros_ec_accel import *
 from cros.tests.cros_ec_gyro import *
@@ -17,8 +16,6 @@ from cros.tests.cros_ec_extcon import *
 class LavaTextTestResult(unittest.TestResult):
     def __init__(self, runner, verbosity=0):
         super().__init__()
-        self.trace_on = verbosity > 0
-        self.debug_on = verbosity > 1
         self.runner = runner
 
     def addSuccess(self, test):
@@ -30,42 +27,18 @@ class LavaTextTestResult(unittest.TestResult):
     def addError(self, test, err):
         super().addError(test, err)
         testcase = test.id().rsplit(".")[-1]
-        if self.trace_on:
-            exc_type, exc_value, exc_tb = err
-            msg = str(exc_value).split(' : ')
-            if len(msg) > 1:
-                msg = ''.join(msg[1:])
-            else:
-                msg = msg[0]
-            self.runner.writeUpdate(f"{testcase} ERROR: {msg}\n")
-        if self.debug_on:
-            exc_type, exc_value, exc_tb = err
-            traceback.print_tb(exc_tb, file=self.runner.stream)
         self.runner.writeUpdate(
             f"<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={testcase} RESULT=unknown>\n")
 
     def addFailure(self, test, err):
         super().addFailure(test, err)
         testcase = test.id().rsplit(".")[-1]
-        if self.trace_on:
-            exc_type, exc_value, exc_tb = err
-            msg = str(exc_value).split(' : ')
-            if len(msg) > 1:
-                msg = ''.join(msg[1:])
-            else:
-                msg = msg[0]
-            self.runner.writeUpdate(f"{testcase} FAIL: {msg}\n")
-        if self.debug_on:
-            exc_type, exc_value, exc_tb = err
-            traceback.print_tb(exc_tb, file=self.runner.stream)
         self.runner.writeUpdate(
             f"<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={testcase} RESULT=fail>\n")
 
     def addSkip(self, test, reason):
         super().addSkip(test, reason)
         testcase = test.id().rsplit(".")[-1]
-        if self.trace_on:
-            self.runner.writeUpdate(f"{testcase} SKIP: {reason}\n")
         self.runner.writeUpdate(
             f"<LAVA_SIGNAL_TESTCASE TEST_CASE_ID={testcase} RESULT=skip>\n")
 
@@ -86,22 +59,8 @@ class LavaTestRunner:
 
 
 if __name__ == "__main__":
-    verbosity = 0
-    # Parse additional "verbosity" parameter and strip it from sys.argv
-    # so that unittest can do the rest of the command line parsing
-    if '--verbosity' in sys.argv:
-        i = sys.argv.index('--verbosity')
-        try:
-            verbosity = int(sys.argv[i+1])
-            sys.argv.pop(i+1)
-        except IndexError:
-            pass
-        except ValueError:
-            sys.argv.pop(i+1)
-        finally:
-            sys.argv.pop(i)
     unittest.main(
-        testRunner=LavaTestRunner(verbosity=verbosity),
+        testRunner=LavaTestRunner(),
         # these make sure that some options that are not applicable
         # remain hidden from the help menu.
         failfast=False,
