34c37f41491d7307cccaa3e28d89f9b62e413746
Tzung-Bi Shih <tzungbi@kernel.org>
2023-03-21 16:23:11 +0800

    cros_ec_accel: simplify test_cros_ec_accel_iio_data_is_valid()
    1. Use glob instead of os.listdir().
    2. Reorder code sequence.
    3. Join multiple lines.
    4. Eliminate try-catch.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>

--- cros/tests/cros_ec_accel.py
+++ cros/tests/cros_ec_accel.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import glob
 import math
-import unittest
 import os
+import unittest
 
 from cros.helpers.kernel import kernel_greater_than
 from cros.helpers.kernel import kernel_lower_than
@@ -55,33 +56,40 @@ class TestCrosECAccel(unittest.TestCase):
         """
         ACCEL_1G_IN_MS2 = 9.8185
         ACCEL_MAG_VALID_OFFSET = 0.25
+        exp = ACCEL_1G_IN_MS2
+        err = exp * ACCEL_MAG_VALID_OFFSET
+
         match = 0
-        try:
-            basepath = "/sys/bus/iio/devices"
-            for devname in os.listdir(basepath):
-                dev_basepath = os.path.join(basepath, devname)
-                with open(os.path.join(dev_basepath, "name")) as fh:
-                    devtype = fh.read()
-                if devtype.startswith("cros-ec-accel"):
-                    with open(os.path.join(dev_basepath, "scale")) as fh:
-                        accel_scale = float(fh.read())
-                    exp = ACCEL_1G_IN_MS2
-                    err = exp * ACCEL_MAG_VALID_OFFSET
-                    mag = 0
-                    for axis in ["in_accel_x_raw",
-                                 "in_accel_y_raw",
-                                 "in_accel_z_raw"]:
-                        axis_path = os.path.join(dev_basepath, axis)
-                        with open(axis_path) as fh:
-                            value = int(fh.read())
-                        value *= accel_scale
-                        mag += value * value
-                    mag = math.sqrt(mag)
-                    self.assertTrue(abs(mag - exp) <= err,
-                                    msg=("Incorrect accelerometer data "
-                                         f"in {dev_basepath} ({abs(mag - exp)})"))
-                    match += 1
-        except IOError as e:
-            self.skipTest(f"{e}")
+        for dev in glob.glob("/sys/bus/iio/devices/*"):
+            p = os.path.join(dev, "name")
+            if not os.path.exists(p):
+                self.skipTest(f"{p} not found")
+            with open(p) as fh:
+                devtype = fh.read()
+            if not devtype.startswith("cros-ec-accel"):
+                continue
+
+            p = os.path.join(dev, "scale")
+            if not os.path.exists(p):
+                self.skipTest(f"{p} not found")
+            with open(p) as fh:
+                accel_scale = float(fh.read())
+
+            mag = 0
+            for axis in ["in_accel_x_raw", "in_accel_y_raw", "in_accel_z_raw"]:
+                axis_path = os.path.join(dev, axis)
+                if not os.path.exists(axis_path):
+                    self.skipTest(f"{axis_path} not found")
+
+                with open(axis_path) as fh:
+                    value = int(fh.read())
+                value *= accel_scale
+                mag += value * value
+            mag = math.sqrt(mag)
+
+            self.assertTrue(abs(mag - exp) <= err,
+                            msg=("Incorrect accelerometer data "
+                                 f"in {dev} ({abs(mag - exp)})"))
+            match += 1
         if match == 0:
             self.skipTest("No accelerometer found")
