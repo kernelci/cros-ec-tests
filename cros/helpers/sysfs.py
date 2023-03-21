5c019323f76733162e1aa8da9991b22a120366da
Tzung-Bi Shih <tzungbi@kernel.org>
2023-03-21 12:09:52 +0800

    helpers/sysfs: eliminate try-catch
    Originally, it used try-catch to check if files don't exist.  The
    try-catch range was too wide.
    
    Two options:
    i) Use try-catch only for open(...).
    ii) Check existence before open(...).
    
    Either way is fine.  Adopt the second option.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>

--- cros/helpers/sysfs.py
+++ cros/helpers/sysfs.py
@@ -10,21 +10,21 @@ def sysfs_check_attributes_exists(s, path, name, files, check_devtype):
         before checking a device path.
     """
     match = 0
-    try:
-        for devname in os.listdir(path):
-            if check_devtype:
-                with open(os.path.join(path, devname, "name")) as fh:
-                    devtype = fh.read()
-                if not devtype.startswith(name):
-                    continue
-            else:
-                if not devname.startswith(name):
-                    continue
-            match += 1
-            for filename in files:
-                p = os.path.join(path, devname, filename)
-                s.assertTrue(os.path.exists(p), msg=f"{p} not found")
-    except IOError as e:
-        s.skipTest(f"{e}")
+    for devname in os.listdir(path):
+        if check_devtype:
+            p = os.path.join(path, devname, "name")
+            if not os.path.exists(p):
+                s.skipTest(f"{p} not found")
+            with open(p) as fh:
+                devtype = fh.read()
+            if not devtype.startswith(name):
+                continue
+        else:
+            if not devname.startswith(name):
+                continue
+        match += 1
+        for filename in files:
+            p = os.path.join(path, devname, filename)
+            s.assertTrue(os.path.exists(p), msg=f"{p} not found")
     if match == 0:
         s.skipTest(f"No {name} found")
