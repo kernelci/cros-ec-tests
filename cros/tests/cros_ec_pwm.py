8806bf749a4c6212042ef65034aa7a1ce2581ec5
Tzung-Bi Shih <tzungbi@kernel.org>
2023-03-22 12:13:09 +0800

    cros_ec_pwm: use RE to parse duty
    To simplify, use RE to parse duty.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>

--- cros/tests/cros_ec_pwm.py
+++ cros/tests/cros_ec_pwm.py
@@ -14,26 +14,31 @@ class TestCrosECPWM(unittest.TestCase):
         """
         if not os.path.exists("/sys/class/backlight/backlight/max_brightness"):
             self.skipTest("No backlight pwm found")
+
         if not os.path.exists("/sys/kernel/debug/pwm"):
             self.skipTest("/sys/kernel/debug/pwm not found")
+
         with open("/sys/kernel/debug/pwm") as fh:
             pwm = fh.read()
         for s in pwm.split("\n\n"):
             if re.match(r".*:ec-pwm.*backlight", s, re.DOTALL):
+                ec_pwm = s
                 break
         else:
             self.skipTest("No EC backlight pwm found")
+
         with open("/sys/class/backlight/backlight/max_brightness") as fh:
             brightness = int(int(fh.read()) / 2)
         with open("/sys/class/backlight/backlight/brightness", "w") as fh:
             fh.write(str(brightness))
-        with open("/sys/kernel/debug/pwm") as fh:
-            for line in fh:
-                if "backlight" in line:
-                    start = line.find("duty") + 6
-                    self.assertNotEqual(start, 5, msg=f"error reading back PWM info: {line}")
-                    end = start + line[start:].find(" ")
-                    self.assertNotEqual(start, end, msg=f"error reading back PWM info: {line}")
-                    duty = int(line[start:end])
-                    self.assertNotEqual(duty, 0, msg=f"error reading back PWM info: {line}")
-                    break
+        for s in ec_pwm.split("\n"):
+            if "backlight" not in s:
+                continue
+
+            m = re.search(r"duty: (\d+)", s)
+            if m:
+                duty = int(m.group(1))
+                self.assertNotEqual(duty, 0, msg="duty should not be 0")
+                break
+        else:
+            self.fail("Failed to parse duty")
