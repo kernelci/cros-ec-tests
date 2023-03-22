cf1d182edff696512bda124bfe636e048c575338
Tzung-Bi Shih <tzungbi@kernel.org>
2023-03-22 10:53:35 +0800

    cros_ec_extcon: simplify test_cros_ec_extcon_usbc_abi()
    - Use glob instead of os.listdir().
    - Eliminate try-catch.
    
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>

--- cros/tests/cros_ec_extcon.py
+++ cros/tests/cros_ec_extcon.py
@@ -1,31 +1,30 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import unittest
+import glob
 import os
+import unittest
 
 
 class TestCrosECextcon(unittest.TestCase):
     def test_cros_ec_extcon_usbc_abi(self):
         """ Checks the cros-ec extcon ABI. """
         match = 0
-        try:
-            basepath = "/sys/class/extcon"
-            for devname in os.listdir(basepath):
-                dev_basepath = os.path.join(basepath, devname)
-                with open(os.path.join(dev_basepath, "name")) as fh:
-                    devtype = fh.read()
-                if ".spi:ec@0:extcon@" in devtype:
-                    p = os.path.join(dev_basepath, "state")
+        for dev in glob.glob("/sys/class/extcon/*"):
+            with open(os.path.join(dev, "name")) as fh:
+                devtype = fh.read()
+            if ".spi:ec@0:extcon@" not in devtype:
+                continue
+
+            p = os.path.join(dev, "state")
+            self.assertTrue(os.path.exists(p), msg=f"{p} not found")
+
+            for cable in os.listdir(dev):
+                if cable.startswith("cable"):
+                    p = os.path.join(dev, cable, "name")
                     self.assertTrue(os.path.exists(p), msg=f"{p} not found")
-                    for cable in os.listdir(dev_basepath):
-                        if cable.startswith("cable"):
-                            p = os.path.join(dev_basepath, cable, "name")
-                            self.assertTrue(os.path.exists(p), msg=f"{p} not found")
-                            p = os.path.join(dev_basepath, cable, "state")
-                            self.assertTrue(os.path.exists(p), msg=f"{p} not found")
-                            match += 1
-        except IOError as e:
-            self.skipTest(f"{e}")
+                    p = os.path.join(dev, cable, "state")
+                    self.assertTrue(os.path.exists(p), msg=f"{p} not found")
+                    match += 1
         if match == 0:
             self.skipTest("No extcon device found")
